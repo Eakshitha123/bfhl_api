66185562d42eb17c8efda69f7fdc16f1b45571f1
Eakshitha <eakshitha2005@gmail.com>
2025-08-29 11:01:23 +0530

    Update test_hosted_api.py

--- test_hosted_api.py
+++ test_hosted_api.py
@@ -4,5 +4,5 @@ url = "https://bfhl-api-82eh.onrender.com/bfhl"
 data = {"data": ["a","1","334","4","R","$"]}
 
 response = requests.post(url, json=data)
-print("Status Code:", response.status_code)  # should print 200
-print("Response JSON:", response.json())     # should print your API response
+print("Status Code:", response.status_code)  
+print("Response JSON:", response.json())   
